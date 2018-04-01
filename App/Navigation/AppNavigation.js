d9e95e141563afce56306ddbdcfef38f161e332d
Sandesh VAKALE <sandesh.vakale@netdevices.fr>
2018-04-01 18:20:37 +0200

    First commit

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,12 @@
 import { StackNavigator } from 'react-navigation'
+import WeatherList from '../Containers/WeatherList'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  WeatherList: { screen: WeatherList },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
