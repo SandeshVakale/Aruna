3f768b5e26948949517d7f94660e92716077890e
Sandesh VAKALE <sandeshvakale@Sandeshs-MacBook-Air.local>
2019-07-11 21:41:33 +0200

    changes to get location

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -7,20 +7,46 @@ import NavigationBar from 'react-native-navbar'
 import Search from '../Components/SearchBox/index'
 
 export class LaunchScreen extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      lastLat: 19.0760, // initial location mumbai
+      lastLong: 72.8777,
+      error: null
+    }
+  }
   onSearch = (searchText) => {
     const {navigate} = this.props.navigation
     navigate('ResultScreen', {searchText: searchText})
   }
 
+  askLocation () {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lastLat: position.coords.latitude,
+          lastLong: position.coords.longitude,
+          error: null
+        })
+        const {homeRequest} = this.props
+        homeRequest(this.state.lastLat, this.state.lastLong)
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    )
+  }
+
   componentDidMount () {
+    this.askLocation()
     const {homeRequest} = this.props
-    homeRequest(19.0760, 72.8777)
+    homeRequest(this.state.lastLat, this.state.lastLong)
   }
   keyExtractor = (item, index) => index
 
   render () {
     const {home} = this.props
-
+    console.log('this.state', this.state)
     if (home.fetching !== true && home.data !== null && home.error === null) {
       return (
         <View style={{ flex: 1, backgroundColor: '#ff9900' }}>
@@ -40,7 +66,7 @@ export class LaunchScreen extends Component {
             data={home.data}
             keyExtractor={this.keyExtractor}
             renderItem={({item}) => <WeatherComponent data={item} {...this.props} />}
-        />
+          />
         </View>
       )
     } else if (home.error !== null) {
